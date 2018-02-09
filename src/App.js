7854e9d4b4b966727549f9b010c11c788f94ff0d
Abin Simon <abinsimon10@gmail.com>
2018-02-09 21:09:07 +0530

    change placeholder text

--- src/App.js
+++ src/App.js
@@ -12,86 +12,61 @@ class App extends Component {
     super(props)
 
     this.state = {
-      likes: ['memes', 'code', 'community', 'develpment', 'AI'],
+      likes: ['talks', 'workshops', 'conferences', 'hackathons', 'cool projects'],
       typing: true, // kinda shady
       doings: [
         {
           name: 'Ask',
-          description:
-            'Got any doubts concerning ML or Deep Learning. Ask us. We are more than happy to answer any of your questions. Maybe add in more stuff here to make it look nicer',
+          description: `So you want to know the feasibility of a project before you start? Is the data you have good enough?
+              Or you have already started and need help? We got you.`,
           button: 'Ask us',
-          link: 'https://keralaai.in'
+          link: 'https://keralaai.in',
         },
         {
-          name: 'Trippledex',
-          description: 'Repair of aneurysm of coronary vessel',
-          button: 'Mauv',
-          link: 'https://keralaai.in'
-        },
-        {
-          name: 'Andalax',
+          name: 'Find Speakers',
           description:
-            'Insertion or replacement of single array rechargeable neurostimulator pulse generator',
-          button: 'Red',
-          link: 'https://keralaai.in'
+            'Hosting a machine learning conference at your campus? We can help you find speakers.',
+          button: 'Hellooo',
+          link: 'https://keralaai.in',
+        },
+        {
+          name: 'Project Sponsorships',
+          description:
+            'Think you are on to something? Show us what you are working on. Expert mentorship and funding for the coolest of the coolest.',
+          button: '$$$',
+          link: 'https://keralaai.in',
         },
       ],
       members: [
         {
-          image: 'https://robohash.org/quaererumneque.jpg?size=100x100&set=set1',
-          email: 'cpiper0@nsw.gov.au',
-          github: 'cpiper0',
-          twitter: 'cpiper0',
-          facebook: 'cpiper0',
-          name: 'Christopher Piper',
+          timestamp: '2018/01/15 5:37:20 PM GMT+5:30',
+          name: 'Gopikrishnan Sasikumar',
+          email: 'gopikrishnans1996@gmail.com',
+          github: 'GopikrishnanSasikumar',
+          twitter: 'gopidotai',
+          facebook: 'Gopikrishnan Sasikumar',
+          photo:
+            '/photos/IMG_20171127_014318_652 - gopi krishnan.jpg',
         },
         {
-          image: 'https://robohash.org/ipsautiure.bmp?size=100x100&set=set1',
-          email: 'bwhitehead1@rakuten.co.jp',
-          github: 'bwhitehead1',
-          twitter: 'bwhitehead1',
-          facebook: 'bwhitehead1',
-          name: 'Brittni Whitehead',
+          timestamp: '2018/01/15 6:03:19 PM GMT+5:30',
+          name: 'Niyas Mohammed',
+          email: 'niaz.angels@gmail.com',
+          github: 'niazangels',
+          twitter: 'niazangels',
+          facebook: 'niazangels',
+          photo:
+            '/photos/niazangels - Niyas Mohammed.jpg',
         },
         {
-          image: 'https://robohash.org/liberosequiatque.jpg?size=100x100&set=set1',
-          email: 'kaleksandrev2@list-manage.com',
-          github: 'kaleksandrev2',
-          twitter: 'kaleksandrev2',
-          facebook: 'kaleksandrev2',
-          name: 'Karin Aleksandrev',
-        },
-        {
-          image: 'https://robohash.org/nisiipsaquae.png?size=100x100&set=set1',
-          email: 'ccostelow3@opensource.org',
-          github: 'ccostelow3',
-          twitter: 'ccostelow3',
-          facebook: 'ccostelow3',
-          name: 'Coletta Costelow',
-        },
-        {
-          image: 'https://robohash.org/ducimusculpaaspernatur.jpg?size=100x100&set=set1',
-          email: 'kdoudny4@pbs.org',
-          github: 'kdoudny4',
-          twitter: 'kdoudny4',
-          facebook: 'kdoudny4',
-          name: 'Keenan Doudny',
-        },
-        {
-          image: 'https://robohash.org/teneturquoest.jpg?size=100x100&set=set1',
-          email: 'mmelmore5@rakuten.co.jp',
-          github: 'mmelmore5',
-          twitter: 'mmelmore5',
-          facebook: 'mmelmore5',
-          name: 'Moreen Melmore',
-        },
-        {
-          image: 'https://robohash.org/temporibusfugitquibusdam.jpg?size=100x100&set=set1',
-          email: 'eshuxsmith6@ovh.net',
-          github: 'eshuxsmith6',
-          twitter: 'eshuxsmith6',
-          facebook: 'eshuxsmith6',
-          name: 'Ethelind Shuxsmith',
+          timestamp: '2018/01/17 9:48:55 PM GMT+5:30',
+          name: 'Praveen Sridhar',
+          email: 'prvn431@gmail.com',
+          github: 'psbots',
+          twitter: 'psbots',
+          facebook: '',
+          photo:
+            '/photos/IMG_20170910_143601 - Praveen Sridhar.jpg',
         },
       ],
     }
@@ -116,15 +91,21 @@ class App extends Component {
             <span>We love </span>
             {this.state.typing ? (
               <Typist onTypingDone={this.typingDone}>
-                code
-                <Typist.Delay ms={500} />
-                <Typist.Backspace count={4} delay={500} />
-                memes
+                talks
                 <Typist.Delay ms={500} />
                 <Typist.Backspace count={5} delay={500} />
-                development
+                workshops
                 <Typist.Delay ms={500} />
-                <Typist.Backspace count={7} delay={700} />
+                <Typist.Backspace count={9} delay={500} />
+                conferences
+                <Typist.Delay ms={500} />
+                <Typist.Backspace count={11} delay={500} />
+                hackathons
+                <Typist.Delay ms={500} />
+                <Typist.Backspace count={10} delay={500} />
+                cool projects
+                <Typist.Delay ms={500} />
+                <Typist.Backspace count={13} delay={700} />
               </Typist>
             ) : (
               ''
@@ -133,10 +114,10 @@ class App extends Component {
         </div>
         <div id="App-section-2">
           <h2 className="App-section-2-tagline">
-            I have no <span className="App-2-tagline-highlight">idea</span> what I am{' '}
-            <span className="App-2-tagline-highlight">doing</span> here. Please, somebody{' '}
-            <span className="App-2-tagline-highlight">help</span> me as soon as you can. I{' '}
-            <span className="App-2-tagline-highlight">really</span> mean it, somebody, anybody.
+            We are an <span className="App-2-tagline-highlight">open community</span> dedicated to
+            advancing <span className="App-2-tagline-highlight">AI research</span> in{' '}
+            <span className="App-2-tagline-highlight">Kerala</span> by bringing together academics,
+            researchers and industry experts alike.
           </h2>
         </div>
         <div id="App-section-3">
